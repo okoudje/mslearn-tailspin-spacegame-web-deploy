b82139ac54a8adf421946e460510ef79223f45bd
JY ABLE <okoudje@outlook.com>
2020-06-29 17:01:29 +0200

    modify dabase schema

--- Tailspin.SpaceGame.Database/dbo/Tables/Profile.sql
+++ Tailspin.SpaceGame.Database/dbo/Tables/Profile.sql
@@ -1,7 +1,7 @@
-﻿
-CREATE TABLE [dbo].[Profiles] (
+﻿CREATE TABLE [dbo].[Profiles] (
     [id]                INT           NOT NULL,
     [userName]          NVARCHAR (50) NOT NULL,
     [avatarUrl]         NVARCHAR (50) NULL,
+    [favoriteMap]       NVARCHAR(50) NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
