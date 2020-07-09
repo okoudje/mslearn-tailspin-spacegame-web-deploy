0e2bcecc3cc6356843fa5986c6fc3020d515f171
JY ABLE <okoudje@outlook.com>
2020-07-09 11:39:54 +0200

    fix tf file

--- main.tf
+++ main.tf
@@ -33,11 +33,18 @@ variable "app_service_name_prefix" {
   description = "The beginning part of your App Service host name"
 }
 
-# Peut etre utile dans certains cas. 
-# resource "random_integer" "app_service_name_suffix" {
-#   min = 1000
-#   max = 9999
-# }
+variable "app_service_map" {  
+  default = {
+     dev_app = "dev"
+     asm_app = "asm"
+     int_app = "int"
+     uat_app = "uat"
+   }
+  
+  description = "List of site"
+}
+
+
 
 resource "azurerm_resource_group" "spacegame" {
   name     = var.resource_group_name
@@ -58,57 +65,52 @@ resource "azurerm_app_service_plan" "spacegame" {
 }
 
 resource "azurerm_app_service" "spacegame" {
-   for_each =    {
-     dev_app = "dev"
-     asm_app = "asm"
-     int_app = "int"
-     uat_app = "uat"
-   }
+   for_each            = tomap(var.app_service_map)     
    name                = "${var.app_service_name_prefix}-${each.value}"
    location            = azurerm_resource_group.spacegame.location
    resource_group_name = azurerm_resource_group.spacegame.name
    app_service_plan_id = azurerm_app_service_plan.spacegame.id
 
-#   site_config {
-#     linux_fx_version = "DOTNETCORE|3.1"
-#     #app_command_line = "dotnet Tailspin.SpaceGame.Web.dll"
-#   }
+  site_config {
+    linux_fx_version = "DOTNETCORE|3.1"
+    #app_command_line = "dotnet Tailspin.SpaceGame.Web.dll"
+  }
 
 }
 
 # appservice_name output
 output "appservice_name_dev" {
-  value       = azurerm_app_service.spacegame.name["dev_app"]
+  value       = azurerm_app_service.spacegame["dev_app"].name
   description = "The App Service name for the dev environment"
 }
 
 output "appservice_name_asm" {
-  value       = azurerm_app_service.spacegame.name["asm_app"]
+  value       = azurerm_app_service.spacegame["asm_app"].name
   description = "The App Service name for the asm environment"
 }
 output "appservice_name_int" {
-  value       = azurerm_app_service.spacegame.name["int_app"]
+  value       = azurerm_app_service.spacegame["int_app"].name
   description = "The App Service name for the int environment"
 }
 output "appservice_name_uat" {
-  value       = azurerm_app_service.spacegame.name["uat_app"]
+  value       = azurerm_app_service.spacegame["uat_app"].name
   description = "The App Service name for the uat environment"
 }
 
 # website_hostname Output
 output "website_hostname_dev" {
-  value       = azurerm_app_service.spacegame_dev.default_site_hostname
+  value       = azurerm_app_service.spacegame["dev_app"].default_site_hostname
   description = "The hostname of the website in the dev environment"
 }
 output "website_hostname_asm" {
-  value       = azurerm_app_service.spacegame_dev.default_site_hostname
+  value       = azurerm_app_service.spacegame["asm_app"].default_site_hostname
   description = "The hostname of the website in the asm environment"
 }
 output "website_hostname_int" {
-  value       = azurerm_app_service.spacegame_dev.default_site_hostname
+  value       = azurerm_app_service.spacegame["int_app"].default_site_hostname
   description = "The hostname of the website in the int environment"
 }
 output "website_hostname_uat" {
-  value       = azurerm_app_service.spacegame_dev.default_site_hostname
+  value       = azurerm_app_service.spacegame["uat_app"].default_site_hostname
   description = "The hostname of the website in the uat environment"
 }
